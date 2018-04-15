b01fd4acb0494281a316eb8c3aa8ae9e6b54a4eb
henryledinhduy <henryledinhduy@gmail.com>
2018-04-16 02:02:56 +0300

    Version 0.1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include "BSTree.h"
 #include <random>
+#include <string>
 
 using namespace std;
 
@@ -15,18 +16,24 @@ using namespace std;
 */
 
 int main(int argc, char const *argv[]) {
+  // Test setting
+  const int RECORD_POPULATION = 500;
+  const int TEST_RUN = 1000;
+  const string file_name = "result.txt";
+  const bool random = true;
+  const bool symmetric = true;
+  const int SEED = 1;
+
   ofstream output;
-  output.open("results.txt");
+  output.open(file_name);
 
   BSTree bst;
   default_random_engine generator;
-  uniform_int_distribution<int> distribution(1,2000);
+  generator.seed(SEED);
+  uniform_int_distribution<int> distribution(1,10000);
 
   int random_number_1;
   int random_number_2;
-
-  const int RECORD_POPULATION = 500;
-  const int TEST_RUN = 100000;
 
   // Establishing the tree
   for (int i = 0; i < RECORD_POPULATION; i++) {
@@ -35,12 +42,13 @@ int main(int argc, char const *argv[]) {
   }
 
   for (int i = 0; i < TEST_RUN; i++) {
-    random_number_1 = distribution(generator);
+    random_number_1 = random ? distribution(generator): i;
     random_number_2 = bst.getRandomNode();
     bst.insert(random_number_1);
 
-    // Change the Deletion Algorithm here to deleteA to use Asymmetric Deletion.
-    bst.deleteS(random_number_2);
+    if (symmetric) bst.deleteS(random_number_2);
+    else bst.deleteA(random_number_2);
+
     bst.updateInfo();
     output << bst.getAPL() << "\n";
   }
